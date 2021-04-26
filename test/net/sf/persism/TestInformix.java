4430b7c93ca4aa9db8500f1b94524541c14a73fa
Dan Howard <sproket@videotron.ca>
2021-04-26 05:40:24 -0400

    Placeholder for Informix test

--- test/net/sf/persism/TestInformix.java
+++ test/net/sf/persism/TestInformix.java
@@ -5,13 +5,25 @@ import junit.framework.TestCase;
 import java.sql.*;
 import java.util.Properties;
 
+// placeholder
 public class TestInformix extends TestCase {
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
 
-        // TODO TestInformix
+
+        Properties props = new Properties();
+        props.load(getClass().getResourceAsStream("/informix.properties"));
+        String driver = props.getProperty("database.driver");
+        String url = props.getProperty("database.url");
+        String username = props.getProperty("database.username");
+        String password = props.getProperty("database.password");
+//        Class.forName(driver);
+
+        url = "jdbc:informix-direct://pinf;user=pinf;password=pinf";
+        Connection con = DriverManager.getConnection(url, "pinf", "pinf");
+
     }
 
     @Override
